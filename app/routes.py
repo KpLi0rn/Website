df067f817e58ad745a61d5b65262efff6965401d
KpLi0rn <563164617@qq.com>
2020-02-02 11:44:16 +0800

    v1.0.1

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template,redirect,flash,url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -18,4 +18,7 @@ def index():
 @app.route('/login',methods=['GET','POST'])
 def login():
     loginform = LoginForm()   # 这里括号没有加 导致出现了错误
-    return render_template("login.html",loginform=loginform)
+    if loginform.validate_on_submit():
+        flash("user{},password{}".format(loginform.username.data,loginform.remember_me.data))
+        return redirect(url_for("index"))
+    return render_template("login.html",loginform=loginform,title="Login in")
