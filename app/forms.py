6c0f3d486f308149e5f83d94c7fbba73e499e0bf
KpLi0rn <563164617@qq.com>
2020-02-05 15:42:57 +0800

    v1.0.4

--- app/forms.py
+++ app/forms.py
@@ -28,7 +28,7 @@ class RegisterForm(FlaskForm):
             raise ValidationError('邮箱已被注册')
 
 # 个人资料的编辑 可以进行user的修改
-class EditForm(FlaskForm):
+class EditForm(FlaskForm):   # 这个form 的类也是继承与 flaskform的
     # 用户名修改的时候我们需要进行查看 我们修改后的用户名 有没有重复 但是如果用户不修改的话 那么也应该允许
     username = StringField("用户名",validators=[DataRequired()])
     about_me = TextAreaField("简介",validators=[length(10,150)])
@@ -38,7 +38,8 @@ class EditForm(FlaskForm):
         # *args 可以传入一系列的数值 数量不定  **kwargs 可以传入字典 一系列
         # 这里为什么要进行一步继承 ?
         # 继承 super(EditForm,self).__init__(*args,**kwargs) 继承父类型的所有参数
-        super(EditForm,self).__init__(*args,**kwargs)  # 这步操作没有看懂?????
+        super(EditForm,self).__init__(*args,**kwargs)  # 如果不使用super那么我们就不能调用父类的属性 这里的EditForm相当于 flaskform的子类
+        # 这行的代码就是 调用EditForm的父类的方法和属性 因为我们要获取的参数是username  username是父类的参数 也就是flaskform的参数
         self.original_username = original_username
 
     def validate_username(self,username):
